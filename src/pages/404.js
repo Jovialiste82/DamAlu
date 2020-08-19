78831c96477a850741bb2c49f1fab872a9fa2273
Philippe Charpentier <jovialiste82@yahoo.fr>
2020-08-18 20:22:17 -0400

    fixed layout issues on about and 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,50 @@
 import React from "react"
-
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import styled from 'styled-components'
+import bgImg from "../images/photo01.jpg"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+
+const Section = styled.section`
+    width: 100%;
+    min-height: 80vh;
+    position: relative;
+    background: linear-gradient(180deg, rgba(111,188,214,1) 0%, rgba(35,124,35,1) 100%);
+    background-image: url(${bgImg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+
+const Text = styled.div`
+    background-color: rgba(0,0,0,.8);
+    color: white;
+    padding: 30vh 5vh 5vh 10vh;
+    min-height: 80vh;
+    width: 100%
+`
+
+
+const NotFoundPage = () => {
+    return (
+        <Layout>
+            <SEO title="404: Wrong direction" />
+            <Section>
+                <Text className="about-page-intro">
+                  
+                  <p>
+                  Je crois bien que vous vous etes trompé de route ....
+                    Mais ce n'est pas bien grave car...
+
+                    ... toutes les routes menent à Rome.   
+                  </p>
+                                  
+                </Text>
+            </Section>
+        </Layout>
+    )
+}
 
 export default NotFoundPage
+
+
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,7 @@ import bgImg from "../images/photo01.jpg"
 
 const Section = styled.section`
     width: 100%;
-    min-height: 120vh;
-    padding: 20vh 5vh 5vh 5vh;
+    min-height: 100vh;
     position: relative;
     background: linear-gradient(180deg, rgba(111,188,214,1) 0%, rgba(35,124,35,1) 100%);
     background-image: url(${bgImg});
@@ -17,14 +16,12 @@ const Section = styled.section`
     background-size: cover;
 `
 
-const Opacity = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
+const Text = styled.div`
     background-color: rgba(0,0,0,.8);
-    z-index: 4;
+    color: white;
+    padding: 20vh 3vh 12vh 3vh;
+    min-height: 100vh;
+    width: 100%
 `
 
 
@@ -33,10 +30,11 @@ const about = () => {
         <Layout>
             <SEO title="About" />
             <Section>
-                <Opacity />
-                <div className="about-page-intro">
-                    Bienvenue chez Dam'Alu. Je me présente, Damien Drame, {(new Date().getFullYear())-1985} ans.J'ai créé l'entreprise Dam'Alu en 2017. Et pour l'instant je travaille seul. Je réalise des menuiseries en aluminium: portes,fenêtres, garde-corps... Pour moi, la menuiserie est une passion que j'exerce depuis bientôt dix ans. En fait, j'y suis arrivé un peu par hasard. J'ai quitté le domicile familial tôt et il fallait bien gagner de l'argent. D'expériences en expériences j'ai découvert la menuiserie.Je m'y suis formé et aujourd'hui, je m’épanouis pleinement dans mon métier. Et puis, j'aime beaucoup la relation client donc c'est parfait. J'accorde beaucoup d'importance aux détails, à la finition. Je penseque c'est ce qui fait ma marque de fabrique.                    
-                </div>
+                <Text>
+                    <div className="about-page-intro">
+                        Bienvenue chez Dam'Alu. Je me présente, Damien Drame, {(new Date().getFullYear())-1985} ans.J'ai créé l'entreprise Dam'Alu en 2017. Et pour l'instant je travaille seul. Je réalise des menuiseries en aluminium: portes,fenêtres, garde-corps... Pour moi, la menuiserie est une passion que j'exerce depuis bientôt dix ans. En fait, j'y suis arrivé un peu par hasard. J'ai quitté le domicile familial tôt et il fallait bien gagner de l'argent. D'expériences en expériences j'ai découvert la menuiserie.Je m'y suis formé et aujourd'hui, je m’épanouis pleinement dans mon métier. Et puis, j'aime beaucoup la relation client donc c'est parfait. J'accorde beaucoup d'importance aux détails, à la finition. Je penseque c'est ce qui fait ma marque de fabrique.                    
+                    </div>
+                </Text>
             </Section>
         </Layout>
     )
